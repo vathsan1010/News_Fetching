c7ebe92a1ceac8bc5e08bf50a8e6d4b71794ca61
vathsan1010 <shreevathsansowmya@gmail.com>
2025-03-06 12:02:17 +0530

    updated in newservice

--- src/services/newsService.js
+++ src/services/newsService.js
@@ -1,50 +1,36 @@
+const API_KEY = "949595b8a0de456a8b6ad9ccb313e46e";
 
-import axios from "axios";
-
-const Api_Key = "949595b8a0de456a8b6ad9ccb313e46e";
-
-const Base_URL="https://newsapi.org/v2/top-headlines";
+const BASE_URL = "https://newsapi.org/v2/top-headlines";
 const EVERYTHING_URL = "https://newsapi.org/v2/everything";
 
+export async function fetchNewsByCategory(category = "general") {
+    try {
+        const response = await fetch(`${BASE_URL}?category=${category}&country=us&apiKey=${API_KEY}`);
+        
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
 
-export async function fetchNewsByCategory(category = "general"){
-    try{
-        const response = await axios.get(`${Base_URL}`,{
-            params:{
-                category,
-                country:"us",
-                apiKey:Api_Key
-            },
-            headers: {
-                "X-Requested-With": "XMLHttpRequest",  
-                "Content-Type": "application/json",
-            },
-        })
-        return response.data.articles
-    }
-    catch(error){
-        console.error("Error fetching news:",error)
-        return[];
+        const data = await response.json();
+        return data.articles;
+    } catch (error) {
+        console.error("Error fetching news:", error);
+        return [];
     }
 }
-export async function fetchNewsBySearch(query)
-{
-    try{
-        const response = await axios.get(`${EVERYTHING_URL}`,{
-            params:{
-                q:query,
-                apiKey:Api_Key
-            },
-            headers: {
-                "X-Requested-With": "XMLHttpRequest", 
-                "Content-Type": "application/json",
-            },
-        })
-        return response.data.articles
-    }
-    catch(error){
-        console.error("Error fetching news:",error)
-        return []
-    }
 
+export async function fetchNewsBySearch(query) {
+    try {
+        const response = await fetch(`${EVERYTHING_URL}?q=${query}&apiKey=${API_KEY}`);
+        
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        return data.articles;
+    } catch (error) {
+        console.error("Error fetching news:", error);
+        return [];
+    }
 }
